e8334cbda7984cd099a974979775040fbb85660d
ella-chetvertak <olegcetvertak037@gmail.com>
2023-11-17 19:47:57 +0200

    .env in security reasons added

--- man/man/settings.py
+++ man/man/settings.py
@@ -12,16 +12,17 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a-l2#kpdpw$n+m-d@q4sjst!)qxr&rqv)^nysq60nu+v5x=*cp'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
